ce34ad7c3cd1fea14ef2c178223c5b26acef3679
Andor Penzes <andor.penzes@gmail.com>
2025-04-27 01:52:17 +0100

    [LEAN] Basic 3.

--- Vol1 - Logical Foundations/Basic.lean
+++ Vol1 - Logical Foundations/Basic.lean
@@ -181,3 +181,267 @@ theorem and_true_elim3
   | true, true  => rfl
   | true, false => contradiction
   | false,  x   => simp [and] at h1
+
+theorem identity_fn_applied_twice
+  (f : Bool -> Bool)
+  (h : forall (x : Bool), f x = x)
+  (b : Bool) :
+  --------------------------------
+  f (f b) = b
+:= by
+  rw [h]
+  rw [h]
+
+theorem manual_grade_for_negative_fn_applied_twice
+  (b : Bool) :
+  !(!b) = b
+:= by
+  simp
+
+theorem andb_eq_orb1
+  (b c : Bool)
+  (h : (b && c) = (b || c)) :
+  ---------------------------
+  b = c
+:= by
+  match b,c with
+  | true , true  => rfl
+  | true , false => contradiction
+  | false, true  => simp [and,or] at h
+  | false, false => rfl
+
+theorem andb_eq_orb2
+  (b c : Bool)
+  (h : (b && c) = (b || c)) :
+  ---------------------------
+  b = c
+:= by
+  cases b <;> cases c <;> try rfl
+  simp [and,or] at h
+  contradiction
+
+inductive Letter where
+  | F | D | C | B | A
+  deriving
+    Repr,
+    DecidableEq,
+    Ord
+
+inductive Modifier where
+  | Minus | Natural | Plus
+  deriving
+    Repr,
+    DecidableEq,
+    Ord
+
+inductive Grade where
+  | Grade (l : Letter) (m : Modifier)
+  deriving
+    Repr,
+    DecidableEq,
+    Ord
+
+inductive Comparison where
+  | Lt | Eq | Gt
+  deriving
+    Repr,
+    DecidableEq
+
+def letterComparison0 (l1 l2 : Letter) : Comparison :=
+  match l1 with
+  | Letter.A => match l2 with
+    | Letter.A => Comparison.Eq
+    | _        => Comparison.Gt
+  | Letter.B => match l2 with
+    | Letter.A => Comparison.Lt
+    | Letter.B => Comparison.Eq
+    | _        => Comparison.Gt
+  | Letter.C => match l2 with
+    | Letter.A
+    | Letter.B => Comparison.Lt
+    | Letter.C => Comparison.Eq
+    | _        => Comparison.Gt
+  | Letter.D => match l2 with
+    | Letter.A
+    | Letter.B
+    | Letter.C => Comparison.Lt
+    | Letter.D => Comparison.Eq
+    | _        => Comparison.Gt
+  | Letter.F => match l2 with
+    | Letter.F => Comparison.Eq
+    | _        => Comparison.Lt
+
+def letterComparison (l1 l2 : Letter) : Ordering := compare l1 l2
+
+theorem letter_comparison_eq
+  (l : Letter) :
+  --------------
+  letterComparison l l = Ordering.eq
+:= by
+  cases l <;> try rfl
+
+def modifierComparison (m1 m2 : Modifier) : Ordering := compare m1 m2
+
+def gradeComparison (g1 g2 : Grade) : Ordering := compare g1 g2
+
+example :
+  (gradeComparison
+    (Grade.Grade Letter.A Modifier.Minus)
+    (Grade.Grade Letter.B Modifier.Plus)
+  = Ordering.gt)
+:= by
+  rfl
+
+example :
+  (gradeComparison
+    (Grade.Grade Letter.A Modifier.Minus)
+    (Grade.Grade Letter.A Modifier.Plus)
+  = Ordering.lt)
+:= by
+  rfl
+
+example :
+  (gradeComparison
+    (Grade.Grade Letter.F Modifier.Plus)
+    (Grade.Grade Letter.F Modifier.Plus)
+  = Ordering.eq)
+:= by
+  rfl
+
+example :
+  (gradeComparison
+    (Grade.Grade Letter.B Modifier.Minus)
+    (Grade.Grade Letter.C Modifier.Plus)
+  = Ordering.gt)
+:= by
+  rfl
+
+def lowerLetter (l : Letter) : Letter :=
+  match l with
+  | Letter.A => Letter.B
+  | Letter.B => Letter.C
+  | Letter.C => Letter.D
+  | Letter.D => Letter.F
+  | Letter.F => Letter.F
+
+theorem lowerLetterIsF :
+  -------------------------------
+  lowerLetter Letter.F = Letter.F
+:= by
+  rfl
+
+theorem lowerLetterLowers
+  (l : Letter)
+  (h : compare Letter.F l = Ordering.lt) :
+  ----------------------------------------
+  letterComparison (lowerLetter l) l = Ordering.lt
+:= by
+  cases l <;> try rfl
+  contradiction
+
+def lowerGrade (g : Grade) : Grade :=
+  match g with
+  | Grade.Grade Letter.F Modifier.Minus => Grade.Grade Letter.F Modifier.Minus
+  | Grade.Grade l m => match m with
+    | Modifier.Plus    => Grade.Grade              l  Modifier.Natural
+    | Modifier.Natural => Grade.Grade              l  Modifier.Minus
+    | Modifier.Minus   => Grade.Grade (lowerLetter l) Modifier.Plus
+
+example :
+  lowerGrade (Grade.Grade Letter.A Modifier.Plus)
+  =
+  Grade.Grade Letter.A Modifier.Natural
+:= by
+  rfl
+
+example :
+  lowerGrade (Grade.Grade Letter.A Modifier.Natural)
+  =
+  Grade.Grade Letter.A Modifier.Minus
+:= by
+  rfl
+
+example :
+  lowerGrade (Grade.Grade Letter.A Modifier.Minus)
+  =
+  Grade.Grade Letter.B Modifier.Plus
+:= by
+  rfl
+
+example :
+  lowerGrade (Grade.Grade Letter.F Modifier.Natural)
+  =
+  Grade.Grade Letter.F Modifier.Minus
+:= by
+  rfl
+
+example :
+  lowerGrade (Grade.Grade Letter.F Modifier.Minus)
+  =
+  Grade.Grade Letter.F Modifier.Minus
+:= by
+  rfl
+
+example :
+  (lowerGrade (lowerGrade (Grade.Grade Letter.B Modifier.Minus)))
+  =
+  Grade.Grade Letter.C Modifier.Natural
+:= by
+  rfl
+
+example :
+  (lowerGrade (lowerGrade (lowerGrade (Grade.Grade Letter.B Modifier.Minus))))
+  =
+  Grade.Grade Letter.C Modifier.Minus
+:= by
+  rfl
+
+theorem lowerGradeLowers
+  (g : Grade)
+  (h : gradeComparison (Grade.Grade Letter.F Modifier.Minus) g = Ordering.lt) :
+  -----------------------------------------------------------------------------
+  gradeComparison (lowerGrade g) g = Ordering.lt
+:= by
+  cases g with
+  | Grade l m =>
+      cases l <;> cases m <;> try rfl
+      contradiction
+
+
+inductive Grd where
+  | GrdDot (l : Letter) (m : Modifier)
+  deriving
+    Repr,
+    DecidableEq,
+    Ord
+
+namespace Grd
+
+def letter (g : Grd) : Letter :=
+  match g with
+  | GrdDot l _ => l
+
+def modifier (g : Grd) : Modifier :=
+  match g with
+  | GrdDot _ m => m
+
+end Grd
+
+-- usage
+#eval (Grd.GrdDot Letter.A Modifier.Plus).letter  -- → Letter.A
+
+structure GradeX where
+  letter   : Letter
+  modifier : Modifier
+  deriving
+    Repr,
+    DecidableEq
+
+structure SuperGradeX extends GradeX where
+  annotation : String
+  deriving
+    Repr,
+    DecidableEq
+
+instance : Coe SuperGradeX GradeX where
+  coe g := { letter := g.letter , modifier := g.modifier }
