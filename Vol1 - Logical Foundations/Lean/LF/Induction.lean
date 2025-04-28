16f21cb97fe74a876223073d2566002140acb172
Andor Penzes <andor.penzes@gmail.com>
2025-04-28 02:46:28 +0100

    [LEAN] Induction 1

--- Vol1 - Logical Foundations/Lean/LF/Induction.lean
+++ Vol1 - Logical Foundations/Lean/LF/Induction.lean
@@ -1,1 +1,49 @@
 import LF.Basic
+
+theorem add_0_r
+  (n : Nat) :
+  -----------
+  n + 0 = n
+:= by
+  cases n <;> rfl
+
+theorem mul_0_r
+  (n : Nat) :
+  -----------
+  (n * 0 = 0)
+:= by
+  cases n <;> rfl
+
+theorem plus_n_Sm
+  (n m : Nat) :
+  -------------
+  Nat.succ (n + m) = n + Nat.succ m
+:= by
+  cases n <;> rfl
+
+theorem add_comm
+  (n m : Nat) :
+  -------------
+  n + m = m + n
+:= by
+  induction n with
+  | zero =>
+      simp
+  | succ n ih =>
+      simp [<- Nat.add_assoc m n 1]
+      rw   [<- ih]
+      rw   [Nat.add_assoc]
+      rw   [<- Nat.succ_eq_one_add]
+      rw   [Nat.add_one]
+      rw   [plus_n_Sm]
+
+theorem add_assoc
+  (n m p : Nat) :
+  ---------------
+  n + (m + p) = (n + m) + p
+:= by
+  induction n with
+  | zero =>
+      simp
+  | succ n ih =>
+      sorry
