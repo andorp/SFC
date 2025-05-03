4724bf384d47babc650c11ac9b77a291a717b85d
Andor Penzes <andor.penzes@gmail.com>
2025-05-03 03:16:07 +0100

    [LEAN] Induction 2

--- Vol1 - Logical Foundations/Lean/LF/Induction.lean
+++ Vol1 - Logical Foundations/Lean/LF/Induction.lean
@@ -46,4 +46,60 @@ theorem add_assoc
   | zero =>
       simp
   | succ n ih =>
-      sorry
+      simp [Nat.succ_add]
+      rw [ih]
+
+def double (n : Nat) : Nat :=
+  match n with
+  | Nat.zero => Nat.zero
+  | Nat.succ m => Nat.succ (Nat.succ (double m))
+
+theorem double_plus
+  (n : Nat) :
+  ----------------
+  double n = n + n
+:= by
+  induction n with
+  | zero =>
+    rfl
+  | succ n ih =>
+    unfold double
+    rw [ih]
+    rw [Nat.succ_add]
+    rfl
+
+theorem mult_plus_distr
+  (n m p : Nat) :
+  -------------------------------
+  (n + m) * p = (n * p) + (m * p)
+:= by
+  induction n with
+  | zero =>
+    simp
+  | succ n ih =>
+    rw [Nat.succ_mul]
+    rw [Nat.add_assoc]
+    rw [Nat.add_comm]
+    rw [Nat.add_assoc (n * p) p (m * p)]
+    rw [Nat.add_comm p (m * p)]
+    rw [<- Nat.add_assoc]
+    rw [<- ih]
+    rw [Nat.add_assoc 1 m n]
+    rw [Nat.succ_add]
+    simp
+    rw [Nat.succ_mul]
+    rw [Nat.add_comm n m]
+
+theorem mult_assoc
+  (n m p : Nat) :
+  -------------------------
+  (n * m) * p = n * (m * p)
+:= by
+  induction n with
+  | zero =>
+    simp
+  | succ n ih =>
+    rw [Nat.succ_mul n (m * p)]
+    rw [Nat.succ_mul n m]
+    rw [mult_plus_distr]
+    rw [ih]
