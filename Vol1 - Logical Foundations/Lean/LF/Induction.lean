77e06c41af2b897bee80c15872afe0a2acd65360
Andor Penzes <andor.penzes@gmail.com>
2025-08-10 21:06:51 +0100

    [LEAN] Induction 3

--- Vol1 - Logical Foundations/Lean/LF/Induction.lean
+++ Vol1 - Logical Foundations/Lean/LF/Induction.lean
@@ -112,8 +112,162 @@ inductive bin where
     Repr ,
     DecidableEq
 
-def binxToNat (b : bin) : Nat :=
+def incrb (b : bin) : bin :=
+  match b with
+  | bin.Z    => bin.B1 bin.Z -- 0   => 2*0 + 1
+  | bin.B0 b => bin.B1 b     -- 2*n => 2*n + 1
+  | bin.B1 b => bin.B0 (incrb b)
+
+def binToNatb (b : bin) : Nat :=
   match b with
   | bin.Z    =>                   0
-  | bin.B0 b =>     2 * binxToNat b
-  | bin.B1 b => 1 + 2 * binxToNat b
+  | bin.B0 b =>     2 * binToNatb b
+  | bin.B1 b => 1 + 2 * binToNatb b
+
+-- Theorem bin_to_nat_incr : forall (b:bin) , bin_to_nat (incr b) = S (bin_to_nat b).
+theorem bin_to_nat_incr
+  (b : bin) :
+  -----------
+  binToNatb (incrb b) = 1 + (binToNatb b)
+:= by
+  induction b with
+  | Z => rfl
+  | B0 b ih => rfl
+  | B1 b ih =>
+    simp [incrb,binToNatb]
+    rewrite [ih]
+    simp [<- Nat.add_assoc, Nat.mul_add]
+
+@[simp]
+def natToBinb (n:Nat) : bin :=
+  match n with
+  | Nat.zero => bin.Z
+  | Nat.succ n => match natToBinb n with
+    | bin.Z => bin.B1 bin.Z
+    | bin.B0 x => bin.B1 x
+    | bin.B1 x => bin.B0 (incrb x)
+
+theorem natBinNat
+  (n : Nat) :
+  -----------
+  binToNatb (natToBinb n) = n
+:= by
+  induction n with
+  | zero => rfl
+  | succ m ih =>
+    cases h1 : (natToBinb m) with
+    | Z =>
+      simp [h1, binToNatb] at ih
+      rw   [<- ih]
+      rfl
+    | B0 b =>
+      simp [h1] at ih
+      simp [h1, binToNatb]
+      rw   [<- ih]
+      simp [binToNatb, Nat.add_comm]
+    | B1 b =>
+      simp [h1,binToNatb] at ih
+      simp [Nat.add_comm]
+      rw   [h1]
+      simp
+      rw [
+        Nat.add_comm,
+        binToNatb,
+        bin_to_nat_incr,
+        <- ih,
+        <- Nat.add_assoc,
+        Nat.mul_add]
+
+theorem double_incr
+  (n : Nat) :
+  -----------
+  double (Nat.succ n) = Nat.succ (Nat.succ (double n))
+:= by
+  rfl
+
+def doubleBin (b : bin) : bin :=
+  match b with
+  | .Z    => .Z
+  | .B0 x => .B0 (.B0 x)
+  | .B1 x => .B0 (.B1 x)
+
+theorem doubleBinIncr
+  (b : bin) :
+  -----------
+  doubleBin (incrb b) = incrb (incrb (doubleBin b))
+:= by
+  cases b <;> rfl
+
+def normalize (b : bin) : bin :=
+  match b with
+  | .Z => .Z
+  | .B0 b => doubleBin (normalize b)
+  | .B1 b => .B1 (normalize b)
+
+theorem incrNatToBin
+  (n : Nat) :
+  -----------
+  (natToBinb (Nat.succ n)) = incrb (natToBinb n)
+:= by
+  match n with
+  | .zero => rfl
+  | .succ n =>
+      simp
+      cases (natToBinb n) <;> rfl
+
+theorem natToBinDouble
+  (n : Nat) :
+  -----------
+  natToBinb (n + n) = doubleBin (natToBinb n)
+:= by
+  induction n with
+  | zero => rfl
+  | succ n ih => rw
+    [ Nat.add_succ
+    , incrNatToBin
+    , Nat.add_comm
+    , <- Nat.add_assoc
+    , Nat.add_succ
+    , incrNatToBin
+    , ih
+    , incrNatToBin
+    , doubleBinIncr
+    ]
+
+theorem doubleAsAdd
+  (n : Nat) :
+  -------------
+  n + n = 2 * n
+:= by
+  induction n with
+  | zero => rfl
+  | succ n ih =>
+    rw
+      [ Nat.add_assoc
+      , Nat.add_comm
+      , Nat.add_comm n 1
+      , <- Nat.add_assoc
+      , Nat.mul_add
+      , <- ih
+      ]
+    simp
+    rw [Nat.add_assoc]
+
+theorem binNatBin
+  (b : bin) :
+  -----------
+  natToBinb (binToNatb b) = normalize b
+:= by
+  induction b with
+  | Z => rfl
+  | B0 b0 ih =>
+    simp [binToNatb, <- doubleAsAdd]
+    rw [natToBinDouble, ih]
+    simp [normalize]
+  | B1 b1 ih =>
+    simp [binToNatb, normalize, <- doubleAsAdd]
+    rw [Nat.succ_add]
+    rw [Nat.add_comm, Nat.add_zero]
+    simp [natToBinb]
+    rw [natToBinDouble, ih]
+    cases (normalize b1) <;> rfl
